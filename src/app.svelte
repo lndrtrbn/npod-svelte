9682b9d844c2cd488d172875b4fcc80c0cc4822c
Landry Trebon <landry.trebon@gmail.com>
2020-10-15 20:30:01 +0200

    WIP: structure the app

---
+++ src/app.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Cursor from "./core/components/cursor/cursor.svelte";
+</script>
+
+<main>
+	<Cursor></Cursor>
+</main>
+
+<style lang="scss">
+	@import "./styles/variables.scss";
+
+	main {
+		height: 100%;
+		background-color: $secondary;
+	}
+</style>
---
+++ src/core/components/cursor/cursor.svelte
@@ -0,0 +1,30 @@
+<main class="host">
+    <div id="small-cursor" class="cursor cursor--small"></div>
+    <canvas id="cursorCanvas" class="cursor cursor--canvas" resize></canvas>
+</main>
+
+<style lang="scss">
+    .host {
+        display: block;
+    }
+    .cursor {
+        position: fixed;
+        left: 0;
+        top: 0;
+        pointer-events: none;
+    }
+    .cursor--small {
+        width: 5px;
+        height: 5px;
+        left: -2.5px;
+        top: -2.5px;
+        border-radius: 50%;
+        z-index: 11000;
+        background: var(--color-text);
+    }
+    .cursor--canvas {
+        width: 100vw;
+        height: 100vh;
+        z-index: 12000;
+    }
+</style>
